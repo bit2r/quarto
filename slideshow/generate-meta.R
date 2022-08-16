8ca3324835f5f27dac4aa3029a659ab1d862ae4b
Kwnaghcun Lee <kwangchun.lee.7@gmail.com>
2022-08-16 23:11:57 +0900

    quarto slide let's go

---
+++ slideshow/generate-meta.R
@@ -0,0 +1,10 @@
+metathis::meta() |> 
+  metathis::meta_social(
+    title = "데이터 사이언스와 디자인",
+    description = "디자인과 아키텍쳐의 중요성",
+    url = "https://jthomasmock.github.io/arrow-dplyr/#/",
+    image = "https://raw.githubusercontent.com/jthomasmock/arrow-dplyr/master/index-img.png",
+    image_alt = "The title slide of the presentation, with the arrow and dplyr hex logos",
+    twitter_card_type = "summary_large_image",
+    twitter_creator = "@thomas_mock"
+  )
