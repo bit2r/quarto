04228ac22f42904c1612aa03fd5cdb4434d4d8a2
Kwangchun Lee <kwangchun.lee.7@gmail.com>
2023-01-03 19:12:01 +0900

    start vs code in R

---
+++ vscode/hello_world.R
@@ -0,0 +1,13 @@
+# 벡터 데이터
+cats <- c(1, 3, 5, 7, 9)
+dogs <- c(8, 6, 4, 2, 1)
+
+# 막대 그래프
+barplot(cats, col="#c9cba6", ylim = c(0,10))
+
+# 선그래프
+lines(dogs, pch=22, lty=2, col="#0631f1", lwd = 3)
+
+# 제목
+title(main = "동물병원 입원 동물수", col.main="red", font.main = 4, cex.main = 3.5)
+
